be6b5ef8f601bcdc8ab5a8229fcba9e2d6063ea2
Ekirapapaul <ekirapapaul@gmail.com>
2021-07-16 13:33:09 +0300

    Add sections G and F to 717

---
+++ openmrs/apps/reports/sql/717_finance.sql
@@ -0,0 +1,27 @@
+SELECT 'G.  FINANCE ' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'G.1  Total amount collected(Ksh) ' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'G.2  Total amount received' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'G.3  Clients waived' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'G.4  Total waived(Ksh) ' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'G.1  Total ExemptedKsh) ' AS 'Service',
+     '' as 'Number'
---
+++ openmrs/apps/reports/sql/717_medical_records.sql
@@ -0,0 +1,12 @@
+SELECT 'F.  MEDICAL RECORDS ISSUED ' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'F.1  New files and folders ' AS 'Service',
+     '' as 'Number'
+
+    UNION ALL
+
+SELECT 'F.2  Outpatient cards/booklets ' AS 'Service',
+     '' as 'Number'
